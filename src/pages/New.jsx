11028d709e4526887db58a18700660be921451f6
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-04-03 03:03:09 +0900

    feat: 일기작성 페이지의 헤더 추가

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+
 const New = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성 페이지 입니다.</p>
+      <MyHeader headText={'새 일기쓰기'} leftChild={<MyButton text={'뒤로가기'} onClick={() => navigate(-1)}/>} />
     </div>
   )
 }
